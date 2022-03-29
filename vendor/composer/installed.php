abbc7d55710d31f77c9a48398cc4ced32fe7d0d5
VaniusaScornici <101570294+VaniusaScornici@users.noreply.github.com>
2022-03-29 18:59:02 +0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '40f5dc588b0439c65c66f9d411cb01f869f333d7',
+        'name' => 'iscornici/to-do-procedure',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'iscornici/to-do-procedure' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '40f5dc588b0439c65c66f9d411cb01f869f333d7',
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-mbstring' => array(
+            'pretty_version' => 'v1.25.0',
+            'version' => '1.25.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/polyfill-mbstring',
+            'aliases' => array(),
+            'reference' => '0abb51d2f102e00a4eefcf46ba7fec406d245825',
+            'dev_requirement' => false,
+        ),
+        'symfony/var-dumper' => array(
+            'pretty_version' => 'v6.0.6',
+            'version' => '6.0.6.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/var-dumper',
+            'aliases' => array(),
+            'reference' => '38358405ae948963c50a3aae3dfea598223ba15e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
